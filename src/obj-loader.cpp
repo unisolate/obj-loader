4dbd46a8b9d36f5dfd2d2b41ee933399d55ca312
unisolate <unisolate@gmail.com>
2016-05-02 21:13:40 +0800

    Add support for v-only format

--- src/obj-loader.cpp
+++ src/obj-loader.cpp
@@ -21,14 +21,17 @@ GLFWwindow* window;
 #include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
-glm::mat4 ViewMatrix;
-glm::mat4 ProjectionMatrix;
+#define W_WIDTH 1280
+#define W_HEIGHT 800
 
-glm::mat4 getViewMatrix() { return ViewMatrix; }
-glm::mat4 getProjectionMatrix() { return ProjectionMatrix; }
+mat4 ViewMatrix;
+mat4 ProjectionMatrix;
+
+mat4 getViewMatrix() { return ViewMatrix; }
+mat4 getProjectionMatrix() { return ProjectionMatrix; }
 
 // Initial position : on +Z
-glm::vec3 position = glm::vec3(0, 0, 5);
+vec3 position = vec3(0, 0, 5);
 // Initial horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // Initial vertical angle : none
@@ -52,23 +55,23 @@ void computeMatricesFromInputs() {
     glfwGetCursorPos(window, &xpos, &ypos);
 
     // Reset mouse position for next frame
-    glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+    glfwSetCursorPos(window, W_WIDTH / 2, W_HEIGHT / 2);
 
     // Compute new orientation
-    horizontalAngle += mouseSpeed * float(1024 / 2 - xpos);
-    verticalAngle += mouseSpeed * float(768 / 2 - ypos);
+    horizontalAngle += mouseSpeed * float(W_WIDTH / 2 - xpos);
+    verticalAngle += mouseSpeed * float(W_HEIGHT / 2 - ypos);
 
     // Direction : Spherical coordinates to Cartesian coordinates conversion
-    glm::vec3 direction(cos(verticalAngle) * sin(horizontalAngle),
-                        sin(verticalAngle),
-                        cos(verticalAngle) * cos(horizontalAngle));
+    vec3 direction(cos(verticalAngle) * sin(horizontalAngle),
+                   sin(verticalAngle),
+                   cos(verticalAngle) * cos(horizontalAngle));
 
     // Right vector
-    glm::vec3 right = glm::vec3(sin(horizontalAngle - 3.14f / 2.0f), 0,
-                                cos(horizontalAngle - 3.14f / 2.0f));
+    vec3 right = vec3(sin(horizontalAngle - 3.14f / 2.0f), 0,
+                      cos(horizontalAngle - 3.14f / 2.0f));
 
     // Up vector
-    glm::vec3 up = glm::cross(right, direction);
+    vec3 up = cross(right, direction);
 
     // Move forward
     if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS) {
@@ -94,14 +97,13 @@ void computeMatricesFromInputs() {
 
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1
     // unit <-> 100 units
-    ProjectionMatrix = glm::perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
+    ProjectionMatrix = perspective(FoV, 4.0f / 3.0f, 0.1f, 100.0f);
     // Camera matrix
-    ViewMatrix =
-        glm::lookAt(position,              // Camera is here
-                    position + direction,  // and looks here : at the same
-                                           // position, plus "direction"
-                    up  // Head is up (set to 0,-1,0 to look upside-down)
-                    );
+    ViewMatrix = lookAt(position,              // Camera is here
+                        position + direction,  // and looks here : at the same
+                                               // position, plus "direction"
+                        up  // Head is up (set to 0,-1,0 to look upside-down)
+                        );
 
     // For the next frame, the "last time" will be "now"
     lastTime = currentTime;
@@ -114,26 +116,23 @@ GLuint LoadShaders(const char* vertex_file_path,
     GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
 
     // Read the Vertex Shader code from the file
-    std::string VertexShaderCode;
-    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
+    string VertexShaderCode;
+    ifstream VertexShaderStream(vertex_file_path, ios::in);
     if (VertexShaderStream.is_open()) {
-        std::string Line = "";
+        string Line = "";
         while (getline(VertexShaderStream, Line))
             VertexShaderCode += "\n" + Line;
         VertexShaderStream.close();
     } else {
-        printf(
-            "Impossible to open %s. Are you in the right directory ? Don't "
-            "forget to read the FAQ !\n",
-            vertex_file_path);
+        fprintf(stderr, "Failed to open shader.\n");
         return 0;
     }
 
     // Read the Fragment Shader code from the file
-    std::string FragmentShaderCode;
-    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
+    string FragmentShaderCode;
+    ifstream FragmentShaderStream(fragment_file_path, ios::in);
     if (FragmentShaderStream.is_open()) {
-        std::string Line = "";
+        string Line = "";
         while (getline(FragmentShaderStream, Line))
             FragmentShaderCode += "\n" + Line;
         FragmentShaderStream.close();
@@ -143,7 +142,7 @@ GLuint LoadShaders(const char* vertex_file_path,
     int InfoLogLength;
 
     // Compile Vertex Shader
-    printf("Compiling shader : %s\n", vertex_file_path);
+    // printf("Compiling shader : %s\n", vertex_file_path);
     char const* VertexSourcePointer = VertexShaderCode.c_str();
     glShaderSource(VertexShaderID, 1, &VertexSourcePointer, NULL);
     glCompileShader(VertexShaderID);
@@ -152,14 +151,14 @@ GLuint LoadShaders(const char* vertex_file_path,
     glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+        vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
         glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL,
                            &VertexShaderErrorMessage[0]);
-        printf("%s\n", &VertexShaderErrorMessage[0]);
+        fprintf(stderr, "%s\n", &VertexShaderErrorMessage[0]);
     }
 
     // Compile Fragment Shader
-    printf("Compiling shader : %s\n", fragment_file_path);
+    // printf("Compiling shader : %s\n", fragment_file_path);
     char const* FragmentSourcePointer = FragmentShaderCode.c_str();
     glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer, NULL);
     glCompileShader(FragmentShaderID);
@@ -168,14 +167,14 @@ GLuint LoadShaders(const char* vertex_file_path,
     glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
     glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+        vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
         glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL,
                            &FragmentShaderErrorMessage[0]);
-        printf("%s\n", &FragmentShaderErrorMessage[0]);
+        fprintf(stderr, "%s\n", &FragmentShaderErrorMessage[0]);
     }
 
     // Link the program
-    printf("Linking program\n");
+    // printf("Linking program\n");
     GLuint ProgramID = glCreateProgram();
     glAttachShader(ProgramID, VertexShaderID);
     glAttachShader(ProgramID, FragmentShaderID);
@@ -185,10 +184,10 @@ GLuint LoadShaders(const char* vertex_file_path,
     glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
     glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
     if (InfoLogLength > 0) {
-        std::vector<char> ProgramErrorMessage(InfoLogLength + 1);
+        vector<char> ProgramErrorMessage(InfoLogLength + 1);
         glGetProgramInfoLog(ProgramID, InfoLogLength, NULL,
                             &ProgramErrorMessage[0]);
-        printf("%s\n", &ProgramErrorMessage[0]);
+        fprintf(stderr, "%s\n", &ProgramErrorMessage[0]);
     }
 
     glDetachShader(ProgramID, VertexShaderID);
@@ -200,69 +199,50 @@ GLuint LoadShaders(const char* vertex_file_path,
     return ProgramID;
 }
 
-bool loadOBJ(const char* path, std::vector<glm::vec3>& out_vertices,
-             std::vector<glm::vec2>& out_uvs,
-             std::vector<glm::vec3>& out_normals) {
-    printf("Loading OBJ file %s...\n", path);
+int loadOBJ(const char* path, vector<vec3>& out_vertices, vector<vec2>& out_uvs,
+            vector<vec3>& out_normals) {
+    // printf("Loading OBJ file %s...\n", path);
 
-    std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
-    std::vector<glm::vec3> temp_vertices;
-    std::vector<glm::vec2> temp_uvs;
-    std::vector<glm::vec3> temp_normals;
+    vector<unsigned int> vertex_idx, uv_idx, normal_idx;
+    vector<vec3> temp_vertices;
+    vector<vec2> temp_uvs;
+    vector<vec3> temp_normals;
 
     FILE* file = fopen(path, "r");
     if (file == NULL) {
-        printf(
-            "Impossible to open the file ! Are you in the right path ? See "
-            "Tutorial 1 for details\n");
-        return false;
+        return -1;
     }
 
-    while (1) {
-        char lineHeader[128];
-        // read the first word of the line
-        int res = fscanf(file, "%s", lineHeader);
-        if (res == EOF) break;  // EOF = End Of File. Quit the loop.
-
-        // else : parse lineHeader
-
-        if (strcmp(lineHeader, "v") == 0) {
-            glm::vec3 vertex;
+    char line[128];
+    while (fscanf(file, "%s", line) != EOF) {
+        if (strcmp(line, "v") == 0) {
+            vec3 vertex;
             fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
             temp_vertices.push_back(vertex);
-        } else if (strcmp(lineHeader, "vt") == 0) {
-            glm::vec2 uv;
+        } else if (strcmp(line, "vt") == 0) {
+            vec2 uv;
             fscanf(file, "%f %f\n", &uv.x, &uv.y);
             uv.y = -uv.y;  // Invert V coordinate since we will only use DDS
                            // texture, which are inverted. Remove if you want to
                            // use TGA or BMP loaders.
             temp_uvs.push_back(uv);
-        } else if (strcmp(lineHeader, "vn") == 0) {
-            glm::vec3 normal;
+        } else if (strcmp(line, "vn") == 0) {
+            vec3 normal;
             fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
             temp_normals.push_back(normal);
-        } else if (strcmp(lineHeader, "f") == 0) {
-            std::string vertex1, vertex2, vertex3;
-            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n",
-                                 &vertexIndex[0], &uvIndex[0], &normalIndex[0],
-                                 &vertexIndex[1], &uvIndex[1], &normalIndex[1],
-                                 &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-            if (matches != 9) {
-                printf(
-                    "File can't be read by our simple parser :-( Try exporting "
-                    "with other options\n");
-                return false;
+        } else if (strcmp(line, "f") == 0) {
+            int sz = (temp_uvs.size() ? 1 : 0) + (temp_normals.size() ? 1 : 0);
+            unsigned int vertex_i, uv_i, normal_i;
+            for (int i = 0; i < 3; ++i) {
+                if (sz) {
+                    fscanf(file, "%d/%d/%d", &vertex_i, &uv_i, &normal_i);
+                    uv_idx.push_back(uv_i);
+                    normal_idx.push_back(normal_i);
+                } else {
+                    fscanf(file, "%d", &vertex_i);
+                }
+                vertex_idx.push_back(vertex_i);
             }
-            vertexIndices.push_back(vertexIndex[0]);
-            vertexIndices.push_back(vertexIndex[1]);
-            vertexIndices.push_back(vertexIndex[2]);
-            uvIndices.push_back(uvIndex[0]);
-            uvIndices.push_back(uvIndex[1]);
-            uvIndices.push_back(uvIndex[2]);
-            normalIndices.push_back(normalIndex[0]);
-            normalIndices.push_back(normalIndex[1]);
-            normalIndices.push_back(normalIndex[2]);
         } else {
             // Probably a comment, eat up the rest of the line
             char stupidBuffer[1000];
@@ -270,48 +250,50 @@ bool loadOBJ(const char* path, std::vector<glm::vec3>& out_vertices,
         }
     }
 
+    int sz = (temp_uvs.size() ? 1 : 0) + (temp_normals.size() ? 1 : 0);
     // For each vertex of each triangle
-    for (unsigned int i = 0; i < vertexIndices.size(); i++) {
+    for (unsigned int i = 0; i < vertex_idx.size(); i++) {
         // Get the indices of its attributes
-        unsigned int vertexIndex = vertexIndices[i];
-        unsigned int uvIndex = uvIndices[i];
-        unsigned int normalIndex = normalIndices[i];
+        unsigned int vertex_i = vertex_idx[i];
 
         // Get the attributes thanks to the index
-        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
-        glm::vec2 uv = temp_uvs[uvIndex - 1];
-        glm::vec3 normal = temp_normals[normalIndex - 1];
+        vec3 vertex = temp_vertices[vertex_i - 1];
 
         // Put the attributes in buffers
         out_vertices.push_back(vertex);
-        out_uvs.push_back(uv);
-        out_normals.push_back(normal);
+
+        if (sz) {
+            unsigned int uv_i = uv_idx[i];
+            unsigned int normal_i = normal_idx[i];
+
+            vec2 uv = temp_uvs[uv_i - 1];
+            vec3 normal = temp_normals[normal_i - 1];
+
+            out_uvs.push_back(uv);
+            out_normals.push_back(normal);
+        }
     }
 
-    return true;
+    return (temp_uvs.size() ? 1 : 0);
 }
 
 int main(void) {
     // Initialise GLFW
     if (!glfwInit()) {
-        fprintf(stderr, "Failed to initialize GLFW\n");
+        fprintf(stderr, "Failed to initialize GLFW.\n");
         return -1;
     }
 
     glfwWindowHint(GLFW_SAMPLES, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
-                   GL_TRUE);  // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // OS X
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Open a window and create its OpenGL context
-    window = glfwCreateWindow(1024, 768, "OBJ Loader", NULL, NULL);
+    window = glfwCreateWindow(W_WIDTH, W_HEIGHT, "OBJ Loader", NULL, NULL);
     if (window == NULL) {
-        fprintf(stderr,
-                "Failed to open GLFW window. If you have an Intel GPU, they "
-                "are not 3.3 compatible. Try the 2.1 version of the "
-                "tutorials.\n");
+        fprintf(stderr, "Failed to open GLFW window.\n");
         glfwTerminate();
         return -1;
     }
@@ -320,7 +302,7 @@ int main(void) {
     // Initialize GLEW
     glewExperimental = true;  // Needed for core profile
     if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
+        fprintf(stderr, "Failed to initialize GLEW.\n");
         glfwTerminate();
         return -1;
     }
@@ -332,10 +314,10 @@ int main(void) {
 
     // Set the mouse at the center of the screen
     glfwPollEvents();
-    glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+    glfwSetCursorPos(window, W_WIDTH / 2, W_HEIGHT / 2);
 
-    // Dark blue background
-    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // White background
+    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
 
     // Enable depth test
     glEnable(GL_DEPTH_TEST);
@@ -363,24 +345,30 @@ int main(void) {
     // GLuint TextureID = glGetUniformLocation(programID, "myTextureSampler");
 
     // Read our .obj file
-    std::vector<glm::vec3> vertices;
-    std::vector<glm::vec2> uvs;
-    std::vector<glm::vec3> normals;  // Won't be used at the moment.
-    bool res = loadOBJ("cube.obj", vertices, uvs, normals);
+    vector<vec3> vertices;
+    vector<vec2> uvs;
+    vector<vec3> normals;  // Won't be used at the moment.
+    int res = loadOBJ("suzanne.obj", vertices, uvs, normals);
+    if (res < 0) {
+        fprintf(stderr, "Failed to parse obj file.\n");
+        return -1;
+    }
 
     // Load it into a VBO
 
     GLuint vertexbuffer;
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec3),
-                 &vertices[0], GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(vec3), &vertices[0],
+                 GL_STATIC_DRAW);
 
     GLuint uvbuffer;
-    glGenBuffers(1, &uvbuffer);
-    glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-    glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0],
-                 GL_STATIC_DRAW);
+    if (res) {
+        glGenBuffers(1, &uvbuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+        glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(vec2), &uvs[0],
+                     GL_STATIC_DRAW);
+    }
 
     do {
         // Clear the screen
@@ -391,10 +379,10 @@ int main(void) {
 
         // Compute the MVP matrix from keyboard and mouse input
         computeMatricesFromInputs();
-        glm::mat4 ProjectionMatrix = getProjectionMatrix();
-        glm::mat4 ViewMatrix = getViewMatrix();
-        glm::mat4 ModelMatrix = glm::mat4(1.0);
-        glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+        mat4 ProjectionMatrix = getProjectionMatrix();
+        mat4 ViewMatrix = getViewMatrix();
+        mat4 ModelMatrix = mat4(1.0);
+        mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
 
         // Send our transformation to the currently bound shader,
         // in the "MVP" uniform
@@ -418,15 +406,17 @@ int main(void) {
                               );
 
         // 2nd attribute buffer : UVs
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-        glVertexAttribPointer(1,         // attribute
-                              2,         // size
-                              GL_FLOAT,  // type
-                              GL_FALSE,  // normalized?
-                              0,         // stride
-                              (void*)0   // array buffer offset
-                              );
+        if (res) {
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+            glVertexAttribPointer(1,         // attribute
+                                  2,         // size
+                                  GL_FLOAT,  // type
+                                  GL_FALSE,  // normalized?
+                                  0,         // stride
+                                  (void*)0   // array buffer offset
+                                  );
+        }
 
         // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, vertices.size());
